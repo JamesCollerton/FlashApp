2677c9103c932c55f97b3fd0ea8ce5f675600b24
James Collerton <jamescollerton@live.co.uk>
2017-08-19 20:54:34 +0100

    Started changing the code in the aperture fragment, need to read up a bit on what it does

--- FlashApp_App/FlashApp/app/src/main/java/com/flashapp/jamescollerton/flashapp/fragments/ApertureFragment.java
+++ FlashApp_App/FlashApp/app/src/main/java/com/flashapp/jamescollerton/flashapp/fragments/ApertureFragment.java
@@ -21,12 +21,12 @@ import com.flashapp.jamescollerton.flashapp.R;
 public class ApertureFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+//    private static final String ARG_PARAM1 = "param1";
+//    private static final String ARG_PARAM2 = "param2";
 
     // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+//    private String mParam1;
+//    private String mParam2;
 
     private OnFragmentInteractionListener mListener;
 
@@ -45,20 +45,20 @@ public class ApertureFragment extends Fragment {
     // TODO: Rename and change types and number of parameters
     public static ApertureFragment newInstance(String param1, String param2) {
         ApertureFragment fragment = new ApertureFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
+//        Bundle args = new Bundle();
+//        args.putString(ARG_PARAM1, param1);
+//        args.putString(ARG_PARAM2, param2);
+//        fragment.setArguments(args);
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+//        if (getArguments() != null) {
+//            mParam1 = getArguments().getString(ARG_PARAM1);
+//            mParam2 = getArguments().getString(ARG_PARAM2);
+//        }
     }
 
     @Override
@@ -69,11 +69,11 @@ public class ApertureFragment extends Fragment {
     }
 
     // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
+//    public void onButtonPressed(Uri uri) {
+//        if (mListener != null) {
+//            mListener.onFragmentInteraction(uri);
+//        }
+//    }
 
     @Override
     public void onAttach(Context context) {
