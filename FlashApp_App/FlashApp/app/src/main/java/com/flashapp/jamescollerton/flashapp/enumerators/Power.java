bbdebff8b06bc75472dcb5c9b6560c18aff6b7a7
James Collerton <jamescollerton@live.co.uk>
2017-10-01 18:12:07 +0100

    Finished the power class

---
+++ FlashApp_App/FlashApp/app/src/main/java/com/flashapp/jamescollerton/flashapp/enumerators/Power.java
@@ -0,0 +1,23 @@
+package com.flashapp.jamescollerton.flashapp.enumerators;
+
+/**
+ * Created by JamesCollerton on 30/09/2017.
+ */
+public enum Power {
+    ZERO(0),
+    MINUS_ONE(1),
+    MINUS_TWO(2),
+    MINUS_THREE(3),
+    MINUS_FOUR(4),
+    MINUS_FIVE(5),
+    MINUS_SIX(6),
+    MINUS_SEVEN(7);
+
+    private final String fraction;
+    private final int value;
+
+    Power(int power){
+        this.fraction = "1/" + (Math.pow(2, power));
+        this.value = -power;
+    }
+}
