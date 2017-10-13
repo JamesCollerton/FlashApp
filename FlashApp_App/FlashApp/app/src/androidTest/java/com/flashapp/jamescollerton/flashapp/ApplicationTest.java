5861939065f64fbc6277c9c3de9fb006b1502c1a
James Collerton <jamescollerton@live.co.uk>
2017-10-13 18:50:26 +0100

    Got all the tests working

--- FlashApp_App/FlashApp/app/src/androidTest/java/com/flashapp/jamescollerton/flashapp/ApplicationTest.java
+++ FlashApp_App/FlashApp/app/src/androidTest/java/com/flashapp/jamescollerton/flashapp/ApplicationTest.java
@@ -67,103 +67,103 @@ public class ApplicationTest {
     Guide Number Tests
      */
 
-//    @Test
-//    public void setGuideNumberInt() {
-//        setGuideNumber("10", "10");
-//    }
-//
-//    @Test
-//    public void setGuideNumberChar() {
-//        setGuideNumber("abcd", "");
-//    }
-//
-//    @Test
-//    public void setGuideNumberCharIntMixedOne() {
-//        setGuideNumber("abcd10", "10");
-//    }
-//
-//    @Test
-//    public void setGuideNumberCharIntMixedTwo() {
-//        setGuideNumber("ab1cd0", "10");
-//    }
-//
-//    @Test
-//    public void setGuideNumberCharSymbol() {
-//        setGuideNumber("!!!!", "1111");
-//    }
-//
-//    @Test
-//    public void setGuideNumberCharSymbolInt() {
-//        setGuideNumber("!1!0!!", "111011");
-//    }
+    @Test
+    public void setGuideNumberInt() {
+        setGuideNumber("10", "10");
+    }
+
+    @Test
+    public void setGuideNumberChar() {
+        setGuideNumber("abcd", "");
+    }
+
+    @Test
+    public void setGuideNumberCharIntMixedOne() {
+        setGuideNumber("abcd10", "10");
+    }
+
+    @Test
+    public void setGuideNumberCharIntMixedTwo() {
+        setGuideNumber("ab1cd0", "10");
+    }
+
+    @Test
+    public void setGuideNumberCharSymbol() {
+        setGuideNumber("!!!!", "1111");
+    }
+
+    @Test
+    public void setGuideNumberCharSymbolInt() {
+        setGuideNumber("!1!0!!", "111011");
+    }
 
     /*
     Distance Tests
      */
 
-//    @Test
-//    public void setDistanceInt() {
-//        setDistance("10", "10");
-//    }
-//
-//    @Test
-//    public void setDistanceChar() {
-//        setDistance("abcd", "");
-//    }
-//
-//    @Test
-//    public void setDistanceCharIntMixedOne() {
-//        setDistance("abcd10", "10");
-//    }
-//
-//    @Test
-//    public void setDistanceCharIntMixedTwo() {
-//        setDistance("ab1cd0", "10");
-//    }
-//
-//    @Test
-//    public void setDistanceCharSymbol() {
-//        setDistance("!!!!", "1111");
-//    }
-//
-//    @Test
-//    public void setDistanceCharSymbolInt() {
-//        setDistance("!1!0!!", "111011");
-//    }
+    @Test
+    public void setDistanceInt() {
+        setDistance("10", "10");
+    }
+
+    @Test
+    public void setDistanceChar() {
+        setDistance("abcd", "");
+    }
+
+    @Test
+    public void setDistanceCharIntMixedOne() {
+        setDistance("abcd10", "10");
+    }
+
+    @Test
+    public void setDistanceCharIntMixedTwo() {
+        setDistance("ab1cd0", "10");
+    }
+
+    @Test
+    public void setDistanceCharSymbol() {
+        setDistance("!!!!", "1111");
+    }
+
+    @Test
+    public void setDistanceCharSymbolInt() {
+        setDistance("!1!0!!", "111011");
+    }
 
     /*
     Aperture Tests
      */
 
-//    @Test
-//    public void setApertureInt() {
-//        setAperture("10", "10");
-//    }
-//
-//    @Test
-//    public void setApertureChar() {
-//        setAperture("abcd", "");
-//    }
-//
-//    @Test
-//    public void setApertureCharIntMixedOne() {
-//        setAperture("abcd10", "10");
-//    }
-//
-//    @Test
-//    public void setApertureCharIntMixedTwo() {
-//        setAperture("ab1cd0", "10");
-//    }
-//
-//    @Test
-//    public void setApertureCharSymbol() {
-//        setAperture("!!!!", "1111");
-//    }
-//
-//    @Test
-//    public void setApertureCharSymbolInt() {
-//        setAperture("!1!0!!", "111011");
-//    }
+    @Test
+    public void setApertureInt() {
+        setAperture("10", "10");
+    }
+
+    @Test
+    public void setApertureChar() {
+        setAperture("abcd", "");
+    }
+
+    @Test
+    public void setApertureCharIntMixedOne() {
+        setAperture("abcd10", "10");
+    }
+
+    @Test
+    public void setApertureCharIntMixedTwo() {
+        setAperture("ab1cd0", "10");
+    }
+
+    @Test
+    public void setApertureCharSymbol() {
+        setAperture("!!!!", "1111");
+    }
+
+    @Test
+    public void setApertureCharSymbolInt() {
+        setAperture("!1!0!!", "111011");
+    }
 
     /*
     Spinner Tests
@@ -189,35 +189,35 @@ public class ApplicationTest {
         setSpinner(input, expectedOutput, R.id.ISO);
     }
 
-//    @Test
-//    public void setISO100() {
-//        setISO("100", "100");
-//    }
-//
-//    @Test
-//    public void setISO200() {
-//        setISO("200", "200");
-//    }
-//
-//    @Test
-//    public void setISO400() {
-//        setISO("400", "400");
-//    }
-//
-//    @Test
-//    public void setISO800() {
-//        setISO("800", "800");
-//    }
-//
-//    @Test
-//    public void setISO1600() {
-//        setISO("1600", "1600");
-//    }
-//
-//    @Test
-//    public void setISO3200() {
-//        setISO("3200", "3200");
-//    }
+    @Test
+    public void setISO100() {
+        setISO("100", "100");
+    }
+
+    @Test
+    public void setISO200() {
+        setISO("200", "200");
+    }
+
+    @Test
+    public void setISO400() {
+        setISO("400", "400");
+    }
+
+    @Test
+    public void setISO800() {
+        setISO("800", "800");
+    }
+
+    @Test
+    public void setISO1600() {
+        setISO("1600", "1600");
+    }
+
+    @Test
+    public void setISO3200() {
+        setISO("3200", "3200");
+    }
 
     /*
     Power
@@ -227,45 +227,45 @@ public class ApplicationTest {
         setCustomSpinner(input, expectedOutput, Power.class, R.id.power);
     }
 
-//    @Test
-//    public void setPowerZero() {
-//        setPower(Power.ZERO, Power.ZERO);
-//    }
+    @Test
+    public void setPowerZero() {
+        setPower(Power.ZERO, Power.ZERO);
+    }
 
-//    @Test
-//    public void setPowerMinusOne() {
-//        setPower(Power.MINUS_ONE, Power.MINUS_ONE);
-//    }
-//
-//    @Test
-//    public void setPowerMinusTwo() {
-//        setPower(Power.MINUS_TWO, Power.MINUS_TWO);
-//    }
-//
-//    @Test
-//    public void setPowerMinusThree() {
-//        setPower(Power.MINUS_THREE, Power.MINUS_THREE);
-//    }
-//
-//    @Test
-//    public void setPowerMinusFour() {
-//        setPower(Power.MINUS_FOUR, Power.MINUS_FOUR);
-//    }
-//
-//    @Test
-//    public void setPowerMinusFive() {
-//        setPower(Power.MINUS_FIVE, Power.MINUS_FIVE);
-//    }
-//
-//    @Test
-//    public void setPowerMinusSix() {
-//        setPower(Power.MINUS_SIX, Power.MINUS_SIX);
-//    }
-//
-//    @Test
-//    public void setPowerMinusSeven() {
-//        setPower(Power.MINUS_SEVEN, Power.MINUS_SEVEN);
-//    }
+    @Test
+    public void setPowerMinusOne() {
+        setPower(Power.MINUS_ONE, Power.MINUS_ONE);
+    }
+
+    @Test
+    public void setPowerMinusTwo() {
+        setPower(Power.MINUS_TWO, Power.MINUS_TWO);
+    }
+
+    @Test
+    public void setPowerMinusThree() {
+        setPower(Power.MINUS_THREE, Power.MINUS_THREE);
+    }
+
+    @Test
+    public void setPowerMinusFour() {
+        setPower(Power.MINUS_FOUR, Power.MINUS_FOUR);
+    }
+
+    @Test
+    public void setPowerMinusFive() {
+        setPower(Power.MINUS_FIVE, Power.MINUS_FIVE);
+    }
+
+    @Test
+    public void setPowerMinusSix() {
+        setPower(Power.MINUS_SIX, Power.MINUS_SIX);
+    }
+
+    @Test
+    public void setPowerMinusSeven() {
+        setPower(Power.MINUS_SEVEN, Power.MINUS_SEVEN);
+    }
 
     /*
     Integration Tests
