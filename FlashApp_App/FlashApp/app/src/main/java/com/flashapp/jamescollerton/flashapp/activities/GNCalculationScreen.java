373a54e4889d347f045d5a3336f49d0f95b973cb
James Collerton <jamescollerton@live.co.uk>
2017-10-23 20:19:52 +0100

    Got rid of menu option, now cleaning up code

--- FlashApp_App/FlashApp/app/src/main/java/com/flashapp/jamescollerton/flashapp/activities/GNCalculationScreen.java
+++ FlashApp_App/FlashApp/app/src/main/java/com/flashapp/jamescollerton/flashapp/activities/GNCalculationScreen.java
@@ -57,27 +57,27 @@ public class GNCalculationScreen    extends     AppCompatActivity
         distanceFragment.addSpinnerItems();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_calculation_screen, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+//        getMenuInflater().inflate(R.menu.menu_calculation_screen, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+//        int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
 
-        return super.onOptionsItemSelected(item);
-    }
+//        return super.onOptionsItemSelected(item);
+//    }
 
     /**
      * Listeners
